80c3de38d13d96ced8cb9eddc1534e89ab350329
Luc Duponcheel <luc.duponcheel@gmail.com>
2013-10-02 22:59:01 +0200

    commit 00.01

---
+++ src/javaz/util/one/One.java
@@ -0,0 +1,11 @@
+package javaz.util.one;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lucd
+ * Date: 10/2/13
+ * Time: 10:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface One {
+}
