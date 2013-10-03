952ae58752f3f8e49a6eb408a80e99ee66df56e2
Luc Duponcheel <luc.duponcheel@gmail.com>
2013-10-03 20:54:24 +0200

    commit 00.01

---
+++ src/javaz/util/function/FunctionFactory.java
@@ -0,0 +1,34 @@
+package javaz.util.function;
+
+/////////////////////////////////////////////////////////////////////////////////
+//      ___________                                         ___________        //
+//     /_______   /\                                       /_______   /\       //
+//     \_____ /  / /  _________   ___     ___     _________\_____ /  / /       //
+//           /  / /  /_____   /\ /__/\   /  /\   /_____   /\     /  / /        //
+//          /  / /  _\____/  / / \  \ \ /  / /  _\____/  / /    /  / /         //
+//         /  / /  /  ___   / /   \  \ /  / /  /  ___   / /    /  / /          //
+//     ___/  / /  /  /__/  / /     \  /  / /  /  /__/  / /    /  /_/______     //
+//    /_____/ /  /________/ /       \/__/ /  /________/ /    /___________/\    //
+//    \_____\/   \________\/         \__\/   \________\/     \___________\/    //
+//                                                                             //
+//                     Java Functional Programming Library                     //
+//                          Luc Duponcheel (ImagineJ)                          //
+/////////////////////////////////////////////////////////////////////////////////
+
+public class FunctionFactory {
+ /**
+  * the identity static method has
+  * a parameter value of type Z
+  * and
+  * a return value of type Z
+  * <p/>
+  * the return value is the same as the parameter value
+  *
+  * @param <Z> the type of the parameter value (and the return value)
+  * @param z   the parameter value
+  * @return return value
+  */
+ public static <Z> Z id(final Z z) {
+  return z;
+ }
+}
